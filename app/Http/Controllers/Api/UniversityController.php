0f9f963938920fbe16c8a5d7ef33aae10ccba1be
BHDHakim <hakim.bhd@gmail.com>
2023-04-28 19:20:43 +0100

    Cache returned universities

--- app/Http/Controllers/Api/UniversityController.php
+++ app/Http/Controllers/Api/UniversityController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class UniversityController extends Controller
 {
     private $BASE_URL = "http://universities.hipolabs.com/search";
     private $MAX_COUNT = 5;
+    private $TTL = 1000 * 60 * 5; // 5 Minutes
 
     /**
      * Display a listing of the resource.
@@ -22,15 +24,19 @@ class UniversityController extends Controller
             'offset' => 0,
         ];
 
-        $unis_by_country = Http::get($this->BASE_URL, [...$shared_query, 'country' => $query])->json();
-        $unis_by_name = Http::get($this->BASE_URL, [...$shared_query, 'name_contains' => $query])->json();
-        $unis_by_domain = Http::get($this->BASE_URL, [...$shared_query, 'domain' => $query])->json();
+        $universities = Cache::remember($query, $this->TTL, function () use ($query, $shared_query) {
+            $unis_by_country = Http::get($this->BASE_URL, [...$shared_query, 'country' => $query])->json();
+            $unis_by_name = Http::get($this->BASE_URL, [...$shared_query, 'name_contains' => $query])->json();
+            $unis_by_domain = Http::get($this->BASE_URL, [...$shared_query, 'domain' => $query])->json();
 
-        $universities = collect([...$unis_by_country, ...$unis_by_name, ...$unis_by_domain])
-            ->map(fn(array $item, int $key) => ["id" => $key, ...$item])
-            ->unique('name')
-            ->slice(0, $this->MAX_COUNT);
+            return collect([...$unis_by_country, ...$unis_by_name, ...$unis_by_domain])
+                ->map(fn(array $item, int $key) => ["id" => $key, ...$item])
+                ->unique('name')
+                ->slice(0, $this->MAX_COUNT)
+                ->values()
+                ->all();
+        });
 
-        return $universities->values()->all();
+        return $universities;
     }
 }
