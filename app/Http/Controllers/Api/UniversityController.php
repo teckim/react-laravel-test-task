d8c85fccd38e3722733ab9d5af85e4e1d946caa2
BHDHakim <hakim.bhd@gmail.com>
2023-05-01 12:49:05 +0100

    Create and use Hipolabs service

--- app/Http/Controllers/Api/UniversityController.php
+++ app/Http/Controllers/Api/UniversityController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Hipolabs\Services\UniversityService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 
 class UniversityController extends Controller
 {
-    private $BASE_URL = "http://universities.hipolabs.com/search";
-    private $MAX_COUNT = 5;
-    private $TTL = 1000 * 60 * 5; // 5 Minutes
+    protected $universityService;
+
+    public function __construct()
+    {
+        $this->universityService = new UniversityService;
+    }
 
     /**
      * Display a listing of the resource.
@@ -19,23 +21,8 @@ class UniversityController extends Controller
     public function index(Request $request)
     {
         $query = $request->query('query');
-        $shared_query = [
-            'limit' => 5,
-            'offset' => 0,
-        ];
 
-        $universities = Cache::remember($query, $this->TTL, function () use ($query, $shared_query) {
-            $unis_by_country = Http::get($this->BASE_URL, [...$shared_query, 'country' => $query])->json();
-            $unis_by_name = Http::get($this->BASE_URL, [...$shared_query, 'name_contains' => $query])->json();
-            $unis_by_domain = Http::get($this->BASE_URL, [...$shared_query, 'domain' => $query])->json();
-
-            return collect([...$unis_by_country, ...$unis_by_name, ...$unis_by_domain])
-                ->map(fn(array $item, int $key) => ["id" => $key, ...$item])
-                ->unique('name')
-                ->slice(0, $this->MAX_COUNT)
-                ->values()
-                ->all();
-        });
+        $universities = $this->universityService->search($query);
 
         return $universities;
     }
